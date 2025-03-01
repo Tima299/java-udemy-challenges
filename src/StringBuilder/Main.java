b64d37e1a809df9f199d62f17b5d9b423d96e93c
Temur <117558961+Tima299@users.noreply.github.com>
2025-03-01 13:06:18 +0500

    This is Temur Eshboyev

---
+++ src/StringBuilder/Main.java
@@ -0,0 +1,29 @@
+package StringBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        String helloWorld = "Hello" + " World";
+        helloWorld.concat(" and Goodbye");
+        StringBuilder helloWorldBuilder = new StringBuilder("Hello" + " World");
+        helloWorldBuilder.append(" and Goodbye");
+
+        printInformation(helloWorld);
+        printInformation(helloWorldBuilder);
+
+        StringBuilder emptyStart = new StringBuilder();
+        emptyStart.append("a".repeat(17));
+        StringBuilder emptyStart32 = new StringBuilder(32);
+        emptyStart32.append("a".repeat(17));
+        printInformation(emptyStart);
+        printInformation(emptyStart32);
+    }
+    public static void printInformation(String string){
+        System.out.println("String = "+string);
+        System.out.println("length = "+string.length());
+    }
+    public static void printInformation(StringBuilder builder){
+        System.out.println("StringBuilder = "+builder);
+        System.out.println("length = " + builder.length());
+        System.out.println("capacity = " + builder.capacity());
+    }
+}
